03c97ed736b94e839018bc4639d1b5dcb13146b4
SaloniJariwala <salonijariwala.developing@gmail.com>
2023-07-06 10:57:51 +0530

    remove APi call

--- pages/products.js
+++ pages/products.js
@@ -3,23 +3,11 @@ import axios from "axios";
 import React from "react";
 
 const Products = () => {
-  return (
-    <FrontLayout>
-      <h1>All Products</h1>
-    </FrontLayout>
-  );
+    return (
+        <FrontLayout>
+            <h1>All Products</h1>
+        </FrontLayout>
+    );
 };
-
-export async function getServerSideProps(context) {
-    const data = [];
-    await axios.get(`http://localhost:1337/api/products?populate=*`).then((respose) => {
-        
-    }).catch((error) => {
-
-    });
-    return {
-        
-    }
-}
 
 export default Products;
